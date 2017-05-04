a572aecb1094d1480ef5b0992d82107b8cc45c59
addykumar27 <adarshna27@yahoo.com>
2017-05-04 14:46:38 -0700

    adding crud to the commentBox.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ router.route('/comments')
   })
 
   .post(function (req,res) {
-    var comment = new comment();
+    var comment = new Comment();
     comment.author = req.body.author;
     comment.text= req.body.text;
 
@@ -74,6 +74,30 @@ router.route('/comments')
 
     });
   })
+
+
+router.route('/comments/:comment_id')
+ .put(function(req, res) {
+   Comment.findById(req.params.comment_id, function(err, comment) {
+     if (err)
+       res.send(err);
+     (req.body.author) ? comment.author = req.body.author : null;
+     (req.body.text) ? comment.text = req.body.text : null;
+     comment.save(function(err) {
+       if (err)
+         res.send(err);
+       res.json({ message: 'Comment has been updated' });
+     });
+   });
+ })
+ //delete method for removing a comment from our database
+ .delete(function(req, res) {
+   Comment.remove({ _id: req.params.comment_id }, function(err, comment) {
+     if (err)
+       res.send(err);
+     res.json({ message: 'Comment has been deleted' })
+   })
+ });
 
 //use router config when we call /API
 app.use('/api', router);
--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,20 +1,86 @@
 import React, { Component } from 'react';
+import $ from 'jquery-ajax';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
-import DATA from '../data';
 import style from './style';
 
 class CommentBox extends Component {
   constructor(props) {
     super(props);
     this.state = { data: [] };
+    this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+    this.handleCommentDelete = this.handleCommentDelete.bind(this);
+    this.handleCommentUpdate = this.handleCommentUpdate.bind(this);
   }
+
+loadCommentsFromServer() {
+    $.ajax({
+      method: 'GET',
+      url: this.props.url
+    })
+    .then((res) => {
+      this.setState({ data: res });
+    }, (err) => {
+      console.log('error', err)
+    })
+  }
+  handleCommentSubmit(comment) {
+      let comments = this.state.data;
+      comment.id = Date.now();
+      let newComments = comments.concat([comment]);
+      this.setState({ data: newComments });
+    $.ajax({
+      method:'POST',
+      url: this.props.url,
+      data: comment
+    })
+      .catch(error => {
+        console.error('post error', error);
+        this.setState({data: comments});
+    });
+  }
+handleCommentDelete(id){
+    $.ajax({
+      method: 'DELETE',
+      url: `${this.props.url}/${id}`
+    })
+    .then((res) => {
+      console.log('Comment deleted');
+    }, (err) => {
+      console.error(err);
+    });
+  }
+
+    handleCommentUpdate(id, comment) {
+    //sends the comment id and new author/text to our api
+    $.ajax({
+      method: 'put',
+      url: `${this.props.url}/${id}`,
+      data: comment
+    })
+    .then(res => {
+      console.log(res);
+    }, err => {
+      console.log(err);
+    })
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  }
+
+
   render() {
     return (
       <div style={ style.commentBox }>
         <h2>Comments:</h2>
-      <CommentList data={ DATA }/>
-      <CommentForm />
+      <CommentList data={this.state.data}
+        onCommentDelete={this.handleCommentDelete}
+        onCommentUpdate={this.handleCommentUpdate}/>
+      <CommentForm  onCommentSubmit={this.handleCommentSubmit}/>
+
       </div>
     )
   }
